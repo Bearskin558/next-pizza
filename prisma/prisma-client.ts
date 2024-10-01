7834a3fc6a1452a40c095ac055353cd054afd28f
bearskin558 <romka558@yandex.ru>
2024-10-01 15:45:29 +0300

    npm

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,27 +1,27 @@
-// import { PrismaClient } from "@prisma/client"
-// const prismaClientSingleton = () => {
-// 	return new PrismaClient()
-// }
-// declare const globalThis: {
-// 	prismaGlobal: ReturnType<typeof prismaClientSingleton>
-// } & typeof global
-// export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
+import { PrismaClient } from "@prisma/client"
+const prismaClientSingleton = () => {
+	return new PrismaClient()
+}
+declare const globalThis: {
+	prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
 
 /*-------------------------------------------*/
-import { Pool, neonConfig } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client"
-import ws from "ws"
+// import { Pool, neonConfig } from "@neondatabase/serverless"
+// import { PrismaNeon } from "@prisma/adapter-neon"
+// import { PrismaClient } from "@prisma/client"
+// import ws from "ws"
 
-// neonConfig.webSocketConstructor = ws
-const neon = new Pool({
-	connectionString: process.env.POSTGRES_PRISMA_URL,
-	connectionTimeoutMillis: 1000,
-	idleTimeoutMillis: 1000,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-})
-const adapter = new PrismaNeon(neon)
-export const prisma = new PrismaClient({ adapter })
+// // neonConfig.webSocketConstructor = ws
+// const neon = new Pool({
+// 	connectionString: process.env.POSTGRES_PRISMA_URL,
+// 	connectionTimeoutMillis: 1000,
+// 	idleTimeoutMillis: 1000,
+// 	ssl: {
+// 		rejectUnauthorized: false,
+// 	},
+// })
+// const adapter = new PrismaNeon(neon)
+// export const prisma = new PrismaClient({ adapter })
